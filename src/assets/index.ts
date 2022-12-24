96b2a9ecaf794525517eb5e23f2b1b96e037ec97
anand pandit <atim4077@gmail.com>
2022-12-25 01:20:40 +0530

    assets and theme added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,7 @@
 import amazonLogo from './amazon-logo.png';
+import realme10 from './products/realme-10-front.jpg';
 
 export {
-    amazonLogo
+    amazonLogo,
+    realme10
 }
---
+++ src/constants/theme.constant.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    primary: 'rgb(157, 212, 222)',
+    secondary: 'rgb(189,248,227)',
+
+}
